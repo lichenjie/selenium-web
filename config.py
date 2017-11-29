e13be5d9fc6ee9e78d58f5e93061a1011f41ed0d
lichenjie <1194222721@qq.com>
2017-11-29 18:32:55 +0800

    add

--- config.py
+++ config.py
@@ -2,37 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SSL_DISABLE = False
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_RECORD_QUERIES = True
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_DEBUG = True
-    MAIL_ASCII_ATTACHMENTS = False
-
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-    FLASKY_MAIL_SUBJECT_PREFIX = '<jacklibra.cn>'
-    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
-    ADMIN = os.environ.get('ADMIN')
-    FLASKY_POSTS_PER_PAGE = 10
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
-    FLASKY_SLOW_DB_QUERY_TIME=0.5
-    SAVEPIC=basedir + '/app/static/images'
-    OSSPIC='http://jacklibra01.oss-cn-shanghai.aliyuncs.com/images'
-    #news
-    NEWS_URL = 'http://ali-news.showapi.com/newsList'
-    APPCODE = 'df4fef021be240aab77b11365132b9ea'
-
-    #thrift
-    GEN_PATH = os.path.join(basedir, 'gen-py')
-
-
     @staticmethod
     def init_app(app):
         pass
@@ -40,18 +9,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'mysql://root:root@127.0.0.1/selenium'
 
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/libra'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/selenium'
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'mysql://root:root@127.0.0.1/selenium'
 
 class HerokuConfig(ProductionConfig):
     SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
